9571baeb8a0887ec40d0343ef5e1a83632800362
Jackie W <95049494+jackiewkr@users.noreply.github.com>
2022-10-28 18:23:51 +0100

    Partial Rewrite to include opaque data types.

--- file.h
+++ file.h
@@ -1,92 +1,30 @@
 #ifndef FILE_H__
 #define FILE_H__
-
 /**
  * file.h
- * ----------
- * Header file for library that handles all file interactions.
- * Stores an object that all file interactions must go through.
+ * ======
+ * Abstract Data Type of a file used within the editor. Split into 'lines' for
+ * ease of use. Also handles file I/O.
+ *
+ * Licensed under GNU GPL v3 license. See LICENSE for more details. 
+ * ----------------------------------------------------------------------------
  */
 
-#define FILENAME_LIM ( 255 ) /* Max length of filenames in most filesystems */
-#define FILE_MIN_SZ  ( 512 ) /* Initial size allocated to f.data (in chars) */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+struct File;
 
-/**
- * struct File
- * ----------
- * Abstract of a file for use with the program. Stores data about the file
- * which can be modified with functions described below.
- * ----------
- * Initialize with init_file(), free with free_file()
- * After initialization, populate file with data with populate_file()
- * Modify file contents with insert_char() and overwrite_char().
- * save file contents to disk with export_file()
- */
-struct File
-{
-        char* filename;
-        char* data;
-        unsigned int sz_alloc; /* Size allocated to f.data (in chars) */
-        unsigned int sz_actual; /* Size actually used of f.data (in chars) */
-};
+void f_destruct( struct File* f );
 
-/*
- * struct File init_file()
- * ----------
- * Initializes and returns a File object with an allocated filename <filename>.
- */
-struct File init_file( char* filename );
+char* f_getLine( struct File* f, int index );
 
-/*
- * void free_file()
- * ----------
- * Frees memory allocated to a given File object <f> and set all other values
- * to 0.
- */
-void free_file( struct File* f );
+int f_getLineLen( struct File* f, int index );
 
-/*
- * void resize_file()
- * ----------
- * Given a pointer to a File object <f>, this function doubles the size of the
- * memory allocated to f.data.
- */
-void resize_file( struct File* f );
+int f_getMaxLines( struct File* f );
 
-/*
- * void populate_file()
- * ----------
- * Populates the data for a file given a <fptr> and a pointer to
- * a File object <f>. Copies the data character by character into f.data,
- * ignoring any non-printable characters (except for newline).
- */
-void populate_file( struct File* f );
-/*
- * void export_file()
- * ----------
- * For a given pointer to a File object <f>, write f.data to a file called
- * f.filename.
- */
-void export_file( struct File* f );
+void f_insertChar( struct File* f, int index, int l_index, int ch );
 
-/*
- * void overwrite_char()
- * ----------
- * Set the character at a given index <index> in the File <f> to the character
- * <ch>. Counterpart to insert_char().
- */
-void overwrite_char( struct File* f, unsigned int index, char ch );
+/** FILE I/O  **/
+struct File* f_import( char* filename, int filename_sz );
 
-/*
- * void insert_char()
- * ----------
- * Move all characters after index <index> to the next index, then set the
- * character at <index> in the File object <f> to <ch>.
- */
-void insert_char( struct File* f, unsigned int index, char ch );
-
+void f_export( struct File* f );
 #endif //FILE_H__
--- main.c
+++ main.c
@@ -8,7 +8,11 @@
  * See LICENSE.txt for licensing information
  */
 
+#include <curses.h>
+#include <string.h>
+
 #include "file.h"
+#include "screen.h"
 
 /*
  * void help()
@@ -18,11 +22,11 @@
  */
 void help()
 {
-        printf( "SBTeditor - Simple Basic Text Editor\n----------\nA text " );
-        printf( "editor written with the goal of being easily understood " );
-        printf( "and modified.\n----------\n    ./sbtedit <filename>: " );
+        printf( "SBTeditor - Simple Basic Text Editor\n----------\nA text "  );
+        printf( "editor written with the goal of being easily understood "   );
+        printf( "and modified.\n----------\n    ./sbtedit <filename>: "      );
         printf( "Opens the file called filename if it exists. Else, create " );
-        printf( "a file called filename and opens it." );
+        printf( "a file called filename and opens it.\n" );
 }
 
 int main( int argc, char* argv[] )
@@ -35,17 +39,52 @@ int main( int argc, char* argv[] )
         }
         char* filename = argv[1];
         /* Open file */
-        struct File f = init_file( filename );
+        struct File* f = f_import( filename, strlen( filename ) );
 
-        populate_file( &f );
+        /* Initialize screen */
+        struct Screen* s = s_construct();
 
+        int ch;
         unsigned int close = 0;
-
         while ( !close )
         {
-                draw_screen();
+                ch = s_update( s, f );
+
+                /* Input parsing */
+                switch ( ch )
+                {
+                case KEY_UP:
+                        s_moveCursor( s, f, -1, 0 );
+                        break;
+                case KEY_DOWN:
+                        s_moveCursor( s, f, 1, 0 );
+                        break;
+                case KEY_LEFT:
+                        s_moveCursor( s, f, 0, -1 );
+                        break;
+                case KEY_RIGHT:
+                        s_moveCursor( s, f, 0, 1 );
+                        break;
+                case KEY_F(10):
+                        close++;
+                        break;
+                case KEY_F(1):
+                        //TODO: save
+                        break;
+                case 10:
+                        s_writeChar( s, f, '\n' );
+                        break;
+                case KEY_BACKSPACE:
+                        s_writeChar( s, f, 127 );
+                        break;
+                default:
+                        s_writeChar( s, f, ch );
+                        break;
+                }
         }
-        
+        s_destruct( s );
+        f_destruct( f );
+        fprintf( stderr, "Closing...\n" );
         return 0;
 }
 
